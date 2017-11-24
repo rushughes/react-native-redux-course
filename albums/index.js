592b213fb3dcd2655da4628e378f6f836ac3583e
Rus Hughes <russell.hughes@gmail.com>
2017-11-24 13:36:05 +0700

    lecture 16

---
+++ albums/index.js
@@ -0,0 +1,8 @@
+// import a library to help create a component
+
+import React from 'react';
+import ReactNative from 'react-native';
+
+// create a components
+
+// render the component to the device
